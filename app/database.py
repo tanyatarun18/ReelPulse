0e874027f8d1b2255f7733857eca84dfcca3ffa0
Tanya Tarun <112303448+tanyatarun18@users.noreply.github.com>
2025-09-27 12:34:40 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -12,7 +12,6 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency to get DB session
 def get_db():
     db = SessionLocal()
     try:
